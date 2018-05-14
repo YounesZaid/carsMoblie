78ef753d7a1e6507ac4a7e07f7621fa50074c869
Younes Zaid <zaidyounes94@gmail.com>
2018-05-14 12:45:57 +0000

    Edited ui

--- App/Trips/TripItem.js
+++ App/Trips/TripItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Button, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ElevatedView from "fiber-react-native-elevated-view";
 
 import * as colors from '_config/colors';
 
@@ -8,23 +9,47 @@ class TripItem extends Component {
   render() {
     const { trip } = this.props;
     return (
-      // <TouchableOpacity
-      //   activeOpacity={.6}
-      //   onPress={e => alert(`${trip.name}`)} >
+      <ElevatedView
+        style={{
+          borderRadius: 2,
+          backgroundColor: colors.white,
+          marginBottom: 10,
+        }}
+        elevation={4}
+        elevationColor={colors.dark}
+        activeElevation={2}>
         <View style={styles.container}>
           <View style={styles.infosContainer}>
-            <Icon name='ios-contact-outline' size={30} color={colors.orange}/>
+            <Icon name='ios-contact-outline' size={30} color={colors.orange} />
             <Text style={styles.textStyle}>{trip.name}</Text>
           </View>
           <View style={styles.infosContainer}>
-            <Icon name='ios-car-outline' size={30}/>
+            <Icon name='ios-car-outline' size={30} />
             <Text style={styles.textStyle}>{trip.carName}</Text>
           </View>
           <View style={styles.infosContainer}>
-            <Icon name='ios-calendar-outline' size={30}/>
+            <Icon name='ios-calendar-outline' size={30} />
             <Text style={styles.textStyle}>{trip.posted}</Text>
           </View>
+          <View style={styles.infosBottom}>
+            <Text>Active</Text>
+            <TouchableOpacity
+              activeOpacity={.6}
+              onPress={e => alert(`${trip.name}`)} >
+            <View style={{
+              flexDirection: 'row',
+            }}>
+              <Text style={{
+                marginRight: 5,
+                fontSize: 16,
+              }}>Track Me</Text>
+              <Icon name='ios-map-outline' size={20} />
+            </View>
+            </TouchableOpacity>
+          </View>
         </View>
+
+      </ElevatedView>
     );
   }
 }
@@ -34,12 +59,8 @@ export default TripItem;
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
-    // alignItems: 'center',
     padding: 15,
     width: '100%',
-    borderRadius: 3,
-    backgroundColor: '#ffffff',
-    marginBottom: 6,
   },
   infosContainer: {
     flexDirection: 'row',
@@ -47,24 +68,25 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginBottom: 5,
   },
+  // infosContainerLast: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   marginLeft: 10,
+  //   marginBottom: 5,
+  //   borderBottomWidth: 1,
+  //   borderBottomColor: '#efefef',
+  // },
   textStyle: {
     fontSize: 18,
-    fontWeight: '500',
+    fontWeight: '400',
     marginLeft: 20
+  },
+  infosBottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#efefef',
   }
 });
 
-{/* <Icon name={'ios-pin-outline'} size={40} style={styles.mapIcon}/>
-          <View>
-            <Text style={styles.textStyle}>{trip.name}</Text>
-            <Text style={styles.textStyle}>{trip.carName}</Text>
-            <Text style={styles.textStyle}>{trip.posted}</Text>
-          </View>
-          <View style={styles.isActiveStyle}>
-            <Text >Active</Text>
-            <Button 
-              onPress={() => {alert('clicked')}}
-              title="Locate Me"
-              color={colors.orange}
-            />
-          </View> */}
